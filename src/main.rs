459130c0cb2877e35237681f8cd20b5b5bc94592
Juhasz Sandor <juhasz.sandor.1987@gmail.com>
2021-10-13 14:52:27 +0200

    command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::{App, Arg, ArgMatches};
+use std::fs::File;
+use std::process;
+
+const APP_NAME:    &'static str = "Payment Engine";
+const APP_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+fn parse_args() -> ArgMatches<'static> {
+    App::new(APP_NAME)
+        .version(APP_VERSION)
+        .arg(Arg::with_name("INPUT")
+            .help("CSV file to use")
+            .required(true)
+        )
+        .arg(Arg::with_name("silent")
+            .short("s")
+            .long("silent")
+            .help("Hide errors")
+        )
+        .get_matches()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = parse_args();
+
+    let filename = opts.value_of("INPUT").expect("missing input arg"); // cannot fail here because it's a required arg
+    let silent = opts.is_present("silent");
+
+    match File::open(filename) {
+        Ok(file) => process_file(file, silent),
+        Err(e) => {
+            if !silent { eprintln!("Opening of file failed! Error: {:?}", e); }
+            process::exit(2)
+        }
+    }
 }
